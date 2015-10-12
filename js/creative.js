9155431c586d479577e8927fa38cc5c1e44b28f9
Michael Zhang <mzhang2016@gmail.com>
2015-10-12 16:20:15 -0400

    Updated 16:20, 10/12

--- js/creative.js
+++ js/creative.js
@@ -30,7 +30,7 @@
     // Fit Text Plugin for Main Header
     $("h1").fitText(
         1.2, {
-            minFontSize: '100px',
+            minFontSize: '100em',
             maxFontSize: '2000px'
         }
     );
